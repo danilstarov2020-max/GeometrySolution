c78bb7f81b4fad54c363a7bcc14ec06449f89ba3
danilstarov2020-max <danilstarov2020@gmail.com>
2026-05-10 13:23:39 +0300

    add CI workflow

---
+++ GeometryApp/Parallelogram.cs
@@ -0,0 +1,19 @@
+﻿namespace GeometryLibrary
+{
+    public class Parallelogram
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double H { get; set; }
+
+        public double Area()
+        {
+            return A * H;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (A + B);
+        }
+    }
+}
---
+++ GeometryConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using GeometryLibrary;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Parallelogram p = new Parallelogram();
+
+        p.A = 5;
+        p.B =4;
+        p.H = 3;
+
+        Console.WriteLine("Площа = " + p.Area());
+        Console.WriteLine("Периметр = " + p.Perimeter());
+    }
+}
